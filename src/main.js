2104bf14fd6b3ee21fb435871ed82579b2fc9109
Jess Liu <jmliu0211@gmail.com>
2025-12-29 17:12:38 -0600

    add glb scene

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
-import * as THREE from 'three';
 import './style.scss'
+import * as THREE from 'three';
+import { GLTFLoader } from 'three/examples/jsm/Addons.js';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
 const canvas = document.querySelector("#experience-canvas");
 const sizes = {
@@ -7,20 +9,29 @@ const sizes = {
   height: window.innerHeight,
 };
 
-
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, sizes.width / sizes.height, 0.1, 1000 );
+const light = new THREE.AmbientLight( 0xfcf8c7, 20 ); // soft white light
+scene.add( light );
+
+const loader = new GLTFLoader();
+loader.load("/models/MeatMyRequests.glb", (glb) => {
+  scene.add(glb.scene);
+});
 
 const renderer = new THREE.WebGLRenderer( {canvas:canvas, antialias:true } );
 renderer.setSize( sizes.width, sizes.height );
 renderer.setPixelRatio( Math.min( window.devicePixelRatio, 2 ) );
 
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+const controls = new OrbitControls( camera, renderer.domElement );
+
+// const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+// const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+// const cube = new THREE.Mesh( geometry, material );
+// scene.add( cube );
 
 camera.position.z = 5;
+controls.update();
 
 // event listeners 
 
@@ -39,11 +50,12 @@ window.addEventListener('resize', () => {
 });
 
 const render = () => {
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
-
+  // cube.rotation.x += 0.01;
+  // cube.rotation.y += 0.01;
+  controls.update();
   renderer.render( scene, camera );
   window.requestAnimationFrame( render );
+  
 }
 
 render();
