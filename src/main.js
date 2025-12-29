2fa112df3139f9087838ca27e43695bed3c59ca3
Jess Liu <jmliu0211@gmail.com>
2025-12-29 16:53:47 -0600

    update when window is resized

--- src/main.js
+++ src/main.js
@@ -1,19 +1,20 @@
 import * as THREE from 'three';
 import './style.scss'
 
-const canvas = document.querySelector('#experience-canvas');
+const canvas = document.querySelector("#experience-canvas");
 const sizes = {
   width: window.innerWidth,
-  height: window.innerHeight
-}
+  height: window.innerHeight,
+};
 
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, sizes.width / sizes.height, 0.1, 1000 );
 
-const renderer = new THREE.WebGLRenderer();
+const renderer = new THREE.WebGLRenderer( {canvas:canvas, antialias:true } );
 renderer.setSize( sizes.width, sizes.height );
-document.body.appendChild( renderer.domElement );
+renderer.setPixelRatio( Math.min( window.devicePixelRatio, 2 ) );
+
 const geometry = new THREE.BoxGeometry( 1, 1, 1 );
 const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
 const cube = new THREE.Mesh( geometry, material );
@@ -21,10 +22,21 @@ scene.add( cube );
 
 camera.position.z = 5;
 
-function animate() {
+// event listeners 
 
+window.addEventListener('resize', () => {
+  // update sizes
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
 
-}
+  // update camera
+  camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix();
+
+  // update renderer
+  renderer.setSize(sizes.width, sizes.height);
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+});
 
 const render = () => {
   cube.rotation.x += 0.01;
