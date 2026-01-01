5163183332cabd4ff2455c40c7061ac6375982cf
Jess Liu <jmliu0211@gmail.com>
2025-12-31 22:04:06 -0600

    setSell, changeSell, remainingMeat, renderPatty

--- src/main.js
+++ src/main.js
@@ -1,4 +1,3 @@
-import { round } from 'three/tsl';
 import './style.scss'
 
 // MEATS list 
@@ -6,28 +5,36 @@ const MEATS = [
     "patty", "sausage", "chickenWing", "chickenThigh","steak"
 ];
 
+// map to convert from DOM element IDs to meat types
+const DOM_TO_MEAT = {
+    "patty": "patty",
+    "sausage": "sausage",
+    "chicken-wing": "chickenWing",
+    "chicken-thigh": "chickenThigh",
+    "steak": "steak"
+};
+
 const state = {
     round: {
-        // price per pound (randomized each round)
-        patty: 2.50,
-        sausage: 3.10,
-        chickenWing: 1.90,
-        chickenThigh: 2.20,
-        steak: 9.75
-    },
-    // available meats this round
-    inventory: {
-        patty: 4,
-        sausage: 12,
-        chickenWing: 18,
-        chickenThigh: 15,
-        steak: 3
-    },
-
-    // persona's requested meats this round
-    requests: [
+        prices: {
+            // price per pound (randomized each round)
+            patty: 2.50,
+            sausage: 3.10,
+            chickenWing: 1.90,
+            chickenThigh: 2.20,
+            steak: 9.75
+        },
+        // available meats this round
+        inventory: {
+            patty: 4,
+            sausage: 12,
+            chickenWing: 18,
+            chickenThigh: 15,
+            steak: 3
+        },
+        requests: [
         {
-            person: "manager",
+            persona: "manager",
             type: "SELL_TOTAL_VALUE_AT_LEAST",
             target: 30 // dollars
         },
@@ -44,6 +51,9 @@ const state = {
             target: 2 // pounds
         }
     ],
+    roundNumber: 1
+},
+    // persona's requested meats this round
     plan: {
         sell: {
             patty: 0,
@@ -55,4 +65,30 @@ const state = {
     }
 };
 
-// round information 
+function setSell(meat, newValue) {
+    const available = state.round.inventory[meat];
+
+    const clampedValue = Math.max(0, Math.min(newValue, available));
+    state.plan.sell[meat] = clampedValue;
+
+}
+
+function changeSell(meat, delta) {
+    // how much we are currently planning to sell
+    const current = state.plan.sell[meat];
+
+    setSell(meat, current + delta);
+}
+
+function remainingMeat(meat) {
+    return state.round.inventory[meat] - state.plan.sell[meat];
+}
+
+function renderPatty(){
+    document.querySelectorAll('.patty-ordered').forEach(el => el.textContent = state.plan.sell.patty);
+    document.getElementById('patty-remaining').textContent = `${remainingMeat("patty")} lbs`;
+}
+
+
+// click action
+
