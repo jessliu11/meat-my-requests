9df108e057b56819314c70623a2e216c685f4d30
Jess Liu <jmliu0211@gmail.com>
2026-01-03 21:24:19 -0600

    update inventory colum new ui

--- src/main.js
+++ src/main.js
@@ -8,18 +8,18 @@ import { changeSell } from './gameLogic.js';
 initializePrices();
 
 // Event listeners
-const meatColumn = document.querySelector('.meat-column');
-meatColumn.addEventListener("click", (event) => {
+const inventoryColumn = document.querySelector('.inventory-column');
+inventoryColumn.addEventListener("click", (event) => {
     const clickedElement = event.target.closest('.plus-sign') || event.target.closest('.minus-sign');
     if (!clickedElement) return;
 
     const clickedPlus = clickedElement.classList.contains('plus-sign');
     const clickedMinus = clickedElement.classList.contains('minus-sign');
 
-    const meatPanel = clickedElement.closest(".meat-panel");
-    if (!meatPanel) return;
+    const orderMeatPanel = clickedElement.closest(".order-meat");
+    if (!orderMeatPanel) return;
 
-    const meat = meatPanel.dataset.meat;
+    const meat = orderMeatPanel.dataset.meat;
     if (!meat) return;
 
     const delta = clickedPlus ? 1 : -1;
