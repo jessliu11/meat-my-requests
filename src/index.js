66ee97516ada446c6f85b3e3228f6a5c926c5bc1
Jess Liu <jmliu0211@gmail.com>
2026-01-05 20:35:47 -0600

    clean up

--- src/index.js
+++ src/index.js
@@ -12,15 +12,19 @@ const sizes = {
 const scene = new THREE.Scene();
 scene.background = new THREE.Color(0xffcdc9);
 const camera = new THREE.PerspectiveCamera( 75, sizes.width / sizes.height, 0.1, 1000 );
-const ambientlight = new THREE.AmbientLight( 0xfcf8c7, 1 ); // soft white light
-const pointLight = new THREE.PointLight( 0xfcf9dc, 150, 500 );
-pointLight.position.set( 0, 5, 5 );
-scene.add( pointLight );
+const ambientlight = new THREE.AmbientLight( 0xfcf8c7, 1.95 ); // soft white light
 scene.add( ambientlight );
+const pointLight1 = new THREE.PointLight( 0xfcf9dc, 100, 100 );
+pointLight1.position.set( 0, 5, 5 );
+scene.add( pointLight1 );
+const pointLight2 = new THREE.PointLight( 0xfcf9dc, 500, 100 );
+pointLight2.position.set( 5, 15, 15 );
+scene.add( pointLight2 );
+
 
 const sphereSize = 1;
-const pointLightHelper = new THREE.PointLightHelper( pointLight, sphereSize );
-scene.add( pointLightHelper );
+// const pointLightHelper = new THREE.PointLightHelper( pointLight, sphereSize );
+// scene.add( pointLightHelper );
 
 
 const loader = new GLTFLoader();
@@ -34,9 +38,14 @@ renderer.setPixelRatio( Math.min( window.devicePixelRatio, 2 ) );
 
 const controls = new OrbitControls( camera, renderer.domElement );
 
+camera.zoom = 1.3;
+camera.updateProjectionMatrix();
 
+camera.position.x = 0;
 camera.position.z = 5;
 camera.position.y = 5;
+
+controls.target.set(0, 0, -2); // Set what the camera looks at
 controls.update();
 
 // event listeners 
